1daa4ea10c275d0498f0cc85da9ee5f623c2dda5
Cynthia Black <cindycblack@gmail.com>
2022-07-23 17:16:13 -0800

    location data stored in local storage

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,3 @@
-// Default location is Anchorage International Aiport
-let latitude = 61.1740847;
-let longitude = -149.9981375;
-//let locationData = `${latitude},${longitude}`;
-
-// Get Geocode.xyz data
 function getGeoCode(location) {
     const geoCode = `https://geocode.xyz/${location}?json=1`;
     fetch(geoCode)
@@ -14,6 +8,18 @@ function getGeoCode(location) {
        let city = data.standard.city;
        let state = data.standard.statename;
        let prov = data.standard.prov;
+  
+       // Print location to DOM
        document.querySelector('#location-name').innerText = `${city}, ${state}, ${prov}`;
+
+       // Store lat / long in local storage
+       let coordinates = {
+        latitude,
+        longitude
+       }
+
+        const coordinatesSerialized = JSON.stringify(coordinates);
+        localStorage.setItem("coordinates", coordinatesSerialized);
     })
-};
+}
+
