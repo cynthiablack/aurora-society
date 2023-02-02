fc18aacd3877e0553ee3c2257869d09430575ad1
Cynthia Black <cindycblack@gmail.com>
2023-02-02 13:09:47 -0900

    Capture user input

--- vite-project/src/components/Navbar.jsx
+++ vite-project/src/components/Navbar.jsx
@@ -1,13 +1,40 @@
 import React from "react"
 
 export default function Navbar() {
+    const [formData, setFormData] = React.useState(
+        {
+            postalCode: ""
+        }
+    )
+
+    function handleChange(event) {
+        setFormData(prevFormData => {
+            return {
+                ...prevFormData,
+                [event.target.name]: event.target.value
+            }
+        })
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault()
+        console.log(formData)
+    }
+
     return (
         <nav>
             <h2 className="nav--title">Anchorage Aurora Society</h2>
-            <div className="nav--search--container">
-                <input type="text" title="location" placeholder="Enter a postal code"></input>
-				<button className="nav--search" id="submit"><img class="nav--icon" src="./src/assets/location.svg" alt="find my location"/></button> 
-            </div>
+            <form className="nav--search--container" onSubmit={handleSubmit}>
+                <input
+                    type="text" 
+                    placeholder="Enter a postal code"
+                    name="postalCode"
+                    onChange={handleChange}
+                    value={formData.postalCode}
+                />
+                <label htmlFor="postalCode" className="form--input--label">Enter a location</label>
+				<button className="form--button">Submit</button> 
+            </form>
         </nav>
     )
 }
