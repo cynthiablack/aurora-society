f1b13beffdc90c85d383a0333653cb038a0dff01
Cynthia Black <cindycblack@gmail.com>
2023-02-03 15:33:52 -0900

    Add KP index, map data to DOM

--- vite-project/src/components/Data.jsx
+++ vite-project/src/components/Data.jsx
@@ -2,18 +2,19 @@ import React from "react"
 import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
 
 function Data(props) {
-    const percentage = props.percentage;
+    const value = props.value;
 
     return (
         <div className="data--contents">
             <CircularProgressbar
-                value={percentage}
-                text={`${percentage}%`}
+                value={value}
+                maxValue={props.maxValue}
+                text={`${value}`}
                 className={props.color}
             />
             <div className="data--text">
-                <h3>Data Title</h3>
-                <p>Additional Data</p>
+                <h3>{props.title}</h3>
+                <p>{props.additionalData}</p>
             </div>
         </div>
     )
